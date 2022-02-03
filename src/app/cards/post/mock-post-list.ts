4ef85f2308edcb8e815604b4cdc97429448b5f3c
shep <ads0041@mix.wvu.edu>
2022-02-03 13:23:18 -0500

    commit for homework, added a post component and linking data

--- src/app/cards/post/mock-post-list.ts
+++ src/app/cards/post/mock-post-list.ts
@@ -1,21 +1,21 @@
 export const mock_post_list = [
   {
     img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiME7C5shpaxl0p_VLzCxXjm7emGHsdzBZhUX5nwo_L1Rg7eNczOQBMCVwo_ilOV8s6rY&usqp=CAU",
-    altImage:"this sucks",
+    altImage:"https://i.kym-cdn.com/photos/images/facebook/001/754/950/555",
     postText: "i hate you, mom",
     name : "ya boi",
   },
   {
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiME7C5shpaxl0p_VLzCxXjm7emGHsdzBZhUX5nwo_L1Rg7eNczOQBMCVwo_ilOV8s6rY&usqp=CAU",
-    altImage:"I love the NSA!",
-    postText: "i hate you, mom",
-    name : " jimbo",
+    img:"https://i.ytimg.com/vi/_TNF-SAlnrQ/maxresdefault.jpg",
+    altImage:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6b232c6b-913f-4340-8f47-1f0f87e50c62/dad4s1u-404ecaff-946d-42f8-9999-6575e9a49845.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZiMjMyYzZiLTkxM2YtNDM0MC04ZjQ3LTFmMGY4N2U1MGM2MlwvZGFkNHMxdS00MDRlY2FmZi05NDZkLTQyZjgtOTk5OS02NTc1ZTlhNDk4NDUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TnqprGPk8Eu-BulOUx8kVyUxEzWTsxOpqjPZtnckq-s",
+    postText: "Making the mother of all omlettes here",
+    name : "Senator Armstrong",
   },
   {
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiME7C5shpaxl0p_VLzCxXjm7emGHsdzBZhUX5nwo_L1Rg7eNczOQBMCVwo_ilOV8s6rY&usqp=CAU",
-    altImage:"Man, im bad at finances",
-    postText: "i hate you, mom",
-    name : "matoi",
+    img:"https://i.kym-cdn.com/entries/icons/original/000/030/079/cover2.jpg",
+    altImage:"https://i.kym-cdn.com/entries/icons/original/000/033/136/cover2.jpg",
+    postText: "Im financially ruined!",
+    name : "Wojak",
   },
 
 ];
--- src/app/cards/post/post.component.ts
+++ src/app/cards/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  img: string;
-  postText: string;
-  altImage: string;
-  name: string;
+
+  @Input() img!: string;
+  @Input() postText!: string;
+  @Input() altImage: string;
+  @Input() name: string;
   constructor() {
     this.altImage = "";
     this.img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiME7C5shpaxl0p_VLzCxXjm7emGHsdzBZhUX5nwo_L1Rg7eNczOQBMCVwo_ilOV8s6rY&usqp=CAU";
